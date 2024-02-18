09653ca729d06a384a63dc5c476611ad674f49ab
Jeremy-YI <yiyanhong10233@gmail.com>
2024-02-18 21:44:12 +1100

    feat:git ignore

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 
 const Projects: FC = () => {
-  return <div className="max-container">Projects</div>
+  return <div className="max-container">Projects coming soon ...</div>
 }
 
 export default Projects
